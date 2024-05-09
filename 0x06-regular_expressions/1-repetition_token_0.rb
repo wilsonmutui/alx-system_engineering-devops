89943d58d5617eaba85250ebd03f360a1ddca345
wilson mutua <patiencechaltu@gmail.com>
2024-05-09 02:19:10 -0700

    regular expression

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -2,4 +2,4 @@
 #accepts one argument and passes to a regular expression
 #The regular expression matches the required characters
 
-puts ARGV[0].scan(/hbt{2,5}n\).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
